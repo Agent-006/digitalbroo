b99a3dd3fa93db28ec99c5b9064616ace7587640
Suman Mondal <mondalsumanrock20@gmail.com>
2024-07-25 02:15:01 +0530

    section:max wrapper added at home page root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 import Hero from "@/app/components/Hero";
 import "./globals.css";
 import Offer from "./components/Offer";
+import { MaxWrapper } from "@/components/shared/MaxWrapper";
 
 export default function Home() {
   return (
-    <div className="w-full px-2 md:px-14 2xl:px-32">
-      <div className="flex flex-col gap-20 md:gap-5 max-w-wrapper">
-        <Hero/>
-        <Offer/>
-        {/* <AboutSection/>
-        <WorkExample/>
-        <Client/> */}
-      </div>
-    </div>
+    <MaxWrapper className="">
+      <Hero />
+    </MaxWrapper>
   );
 }
