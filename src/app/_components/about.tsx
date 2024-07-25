3efa1d5c9d1007b48039c0e9fc76897c99b3db6f
gourab8389 <deyg6988@gmail.com>
2024-07-25 19:56:55 +0530

    refactor: hero

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -7,15 +7,15 @@ const About = () => {
     <div className="h-[80vh] w-full py-10 flex items-center">
       <div className="w-full h-full flex items-center justify-between px-10">
         <div className="w-[50%] h-full relative">
-            <div className="w-60 h-40 bg-light shadow-xl absolute top-0 left-0 rounded-lg">
-card1
-            </div>
-            <div className="w-60 h-40 bg-light shadow-xl absolute bottom-0 left-0 rounded-lg">
-card2
-            </div>
-            <div className="w-[14rem] h-[15rem] bg-light shadow-xl absolute top-24 right-14 rounded-lg">
-card3
-            </div>
+          <div className="w-60 h-40 bg-light shadow-xl absolute top-0 left-0 rounded-lg">
+            card1
+          </div>
+          <div className="w-60 h-40 bg-light shadow-xl absolute bottom-0 left-0 rounded-lg">
+            card2
+          </div>
+          <div className="w-[14rem] h-[15rem] bg-light shadow-xl absolute top-24 right-14 rounded-lg">
+            card3
+          </div>
         </div>
         <div className="w-[50%] flex flex-col space-y-6 items-start justify-start">
           <h1 className="text-5xl font-bold text-dark">About Us</h1>
